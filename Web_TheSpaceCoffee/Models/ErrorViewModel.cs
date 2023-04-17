5869c95e2b6d17978a0d25fe7e3e958ad1cdf0b5
Nguyen Quy <dinhquy4869@gmail.com>
2023-04-17 13:49:35 +0700

    Add project files.

---
+++ Web_TheSpaceCoffee/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web_TheSpaceCoffee.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
